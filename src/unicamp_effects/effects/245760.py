aad9680b6ce8f972fba81c1688fb4df59e42b734
joaosenirio <senirio10@gmail.com>
2026-04-03 16:25:19 -0300

    [Add] Adicionando efeito de aberração cromática

--- src/unicamp_effects/effects/245760.py
+++ src/unicamp_effects/effects/245760.py
@@ -1,8 +1,69 @@
+import cv2
 import numpy as np
 from PIL import Image
 
 from unicamp_effects.registry import register
 
+# --- Funções auxiliares para aberração cromática
+def _shift_channel(channel: np.ndarray, shift_x: int) -> np.ndarray:
+    h, w = channel.shape
+
+    if shift_x == 0:
+        return channel
+
+    if shift_x > 0:
+        padded = np.pad(channel, ((0, 0), (shift_x, 0)), mode="edge")
+        return padded[:, :w]
+
+    offset = -shift_x
+    padded = np.pad(channel, ((0, 0), (0, offset)), mode="edge")
+    return padded[:, offset:offset + w]
+
+def _mascara_roxo(img: np.ndarray) -> np.ndarray:
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    h = hsv[:, :, 0]
+    s = hsv[:, :, 1]
+    v = hsv[:, :, 2]
+
+    h_min, h_max = 120, 170
+    s_min, s_max = 70, 255
+
+    # mascara binaria 
+    mask = np.zeros(h.shape, dtype=np.uint8)
+    mask[
+        (h >= h_min) & (h <= h_max) &
+        (s >= s_min) & (s <= s_max)] = 255
+
+    # fundo em grayscale
+    # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # background = np.repeat(gray[:, :, np.newaxis], 3, axis=2)
+
+    return mask
+
+@register(prefix="245760")
+def aberracao_cromatica(img: np.ndarray) -> np.ndarray:
+    mask = _mascara_roxo(img)
+
+    img_segmentada = np.zeros_like(img)
+    img_segmentada[mask == 255] = img[mask == 255]
+    
+    # aberracao cromatica
+    shift = 8
+    red = _shift_channel(img_segmentada[:, :, 0], shift)
+    green = img_segmentada[:, :, 1]
+    blue = _shift_channel(img_segmentada[:, :, 2], -shift)
+    aberrated = np.stack([red, green, blue], axis=2)
+
+    # fundo em grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    background = np.repeat(gray[:, :, np.newaxis], 3, axis=2)
+
+    result = np.zeros_like(img)
+    result[mask == 0] = background[mask == 0]
+    result[mask == 255] = aberrated[mask == 255]
+
+    return result.astype(np.uint8)
 
 @register(prefix="245760")
 def dithering(img: np.ndarray) -> np.ndarray:
